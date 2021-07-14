67c3e573a110fa5c0746bd5d678400f8837a4fb7
BBP <bishantpant2010@gmail.com>
2021-07-14 13:09:24 +0545

    button animation tweaked and xhr request added

--- src/index.js
+++ src/index.js
@@ -12,8 +12,34 @@ btnClicked = (btn) => {
   if (queryInput.value === "") {
     console.error("invalid input");
     queryInput.placeholder = "NO query my guy :(";
+  } else {
+    request();
   }
   btn.addEventListener("blur", () => {
     queryInput.placeholder = "";
   });
+};
+
+displayResponse = (responseJSON) => {
+  responseOBJ = JSON.parse(responseJSON);
+  console.log(responseOBJ.data[1].displayName);
+};
+
+request = () => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://valorant-api.com/v1/weapons", true);
+
+  xhr.onprogress = function () {
+    console.log("on progress");
+  };
+
+  xhr.onload = function () {
+    console.log(this.status);
+    if (this.status === 200) {
+      displayResponse(this.responseText);
+    }
+  };
+
+  xhr.send();
 };
