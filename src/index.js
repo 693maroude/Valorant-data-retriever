1d66848e663d465bf493ac7ebd594ae609f872a3
BBP <bishantpant2010@gmail.com>
2021-07-16 14:31:57 +0545

    agent data is now fetched from server itself

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-let agentUuid = {
-  breach: "5f8d3a7f-467b-97f3-062c-13acf203c006",
-  raze: "f94c3b30-42be-e959-889c-5aa313dba261",
-  kayo: "601dbbe7-43ce-be57-2a40-4abd24953621",
-  skye: "6f2a04ca-43e0-be17-7f36-b3908627744d",
-  cypher: "117ed9e3-49f3-6512-3ccf-0cada7e3823b",
-  sova: "ded3520f-4264-bfed-162d-b080e2abccf9",
-  killjoy: "1e58de9c-4950-5125-93e9-a0aee9f98746",
-  viper: "707eab51-4836-f488-046a-cda6bf494859",
-  phoenix: "eb93336a-449b-9c1b-0a54-a891f7921d69",
-  astra: "41fb69c1-4189-7b37-f117-bcaf1e96f1bf",
-  brimestone: "9f0d8ba9-4140-b941-57d3-a7ad57c6b417",
-  yoru: "7f94d92c-4234-0a36-9646-3a87eb8b5c89",
-  sage: "569fdd95-4d10-43ab-ca70-79becc718b46",
-  reyna: "a3bfb853-43b2-7238-a4f1-ad90e9e46bcc",
-  omen: "8e253930-4c05-31dd-1b6c-968525494517",
-  jett: "add6443a-41bd-e414-f6ad-e58d267f4e95",
+let agentUuid = [];
+
+window.onload = () => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://valorant-api.com/v1/agents/", true);
+  xhr.send();
+  xhr.onload = function () {
+    let agentData = JSON.parse(this.responseText);
+    agentData.data.forEach((element) => {
+      agentUuid.push(element.developerName);
+      agentUuid.push(element.displayName.toLowerCase());
+      agentUuid.push(element.uuid);
+    });
+    let filterIndex = agentUuid.indexOf("Hunter_NPE");
+    agentUuid.splice(filterIndex, 3);
+    console.log(agentUuid);
+  };
 };
 
 queryInput.addEventListener("focus", () => {
@@ -27,6 +28,10 @@ queryInput.addEventListener("blur", () => {
   }
 });
 
+sendRequest.addEventListener("blur", () => {
+  queryInput.placeholder = "";
+});
+
 btnClicked = (btn) => {
   if (queryInput.value === "") {
     console.error("invalid input");
@@ -34,9 +39,6 @@ btnClicked = (btn) => {
   } else {
     request(queryInput.value);
   }
-  btn.addEventListener("blur", () => {
-    queryInput.placeholder = "";
-  });
 };
 
 displayResponse = (responseJSON) => {
@@ -62,23 +64,31 @@ displayResponse = (responseJSON) => {
 request = (query) => {
   const xhr = new XMLHttpRequest();
   query = query.toLowerCase();
+  let queryIndex = agentUuid.indexOf(query);
 
-  xhr.open(
-    "GET",
-    `https://valorant-api.com/v1/agents/${agentUuid[query]}`,
-    true
-  );
+  if (queryIndex === -1) {
+    queryInput.placeholder = "No such agent exists";
+  } else {
+    xhr.open(
+      "GET",
+      `https://valorant-api.com/v1/agents/${agentUuid[queryIndex + 1]}`,
+      true
+    );
 
-  xhr.onprogress = function () {
-    console.log("on progress");
-  };
+    xhr.onprogress = function () {
+      console.log("on progress");
+    };
 
-  xhr.onload = function () {
-    console.log(this.status);
-    if (this.status === 200) {
-      displayResponse(this.responseText);
-    }
-  };
+    xhr.onload = function () {
+      console.log(this.status);
+      if (this.status === 200) {
+        displayResponse(this.responseText);
+      } else {
+        console.error(this.status);
+        console.error(this.statusText);
+      }
+    };
 
-  xhr.send();
+    xhr.send();
+  }
 };
